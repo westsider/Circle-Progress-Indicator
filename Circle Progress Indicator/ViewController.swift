327cb4563b3bc68a46ff716bda7753c2df4882e1
Warren Hansen <whansen1@me.com>
2018-08-03 09:51:40 -0700

    initial commit

--- Circle Progress Indicator/ViewController.swift
+++ Circle Progress Indicator/ViewController.swift
@@ -10,16 +10,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let progressShape = CAShapeLayer()
+    let backgroundShape = CAShapeLayer()
+    var percent = 0.5
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        view.layer.addSublayer(backgroundShape)
+        view.layer.addSublayer(progressShape)
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        updateIndicator(with: percent, isAnimated: true)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func updateIndicator(with percent: Double, isAnimated: Bool = false) {
+        let shortestSide = min(view.frame.size.width, view.frame.size.height)
+        let strokeWidth: CGFloat = 40.0
+        let frame = CGRect(x: 0, y: 0, width: shortestSide - strokeWidth, height: shortestSide - strokeWidth)
+        backgroundShape.frame = frame
+        backgroundShape.path = UIBezierPath(ovalIn: backgroundShape.frame).cgPath
+        backgroundShape.position = view.center
+        backgroundShape.strokeColor = UIColor.black.cgColor
+        backgroundShape.lineWidth = strokeWidth
+        backgroundShape.fillColor = UIColor.clear.cgColor
+        
+        progressShape.frame = frame
+        progressShape.path = backgroundShape.path
+        progressShape.position = backgroundShape.position
+        progressShape.strokeColor = UIColor.red.cgColor
+        progressShape.lineWidth = backgroundShape.lineWidth
+        progressShape.fillColor = UIColor.clear.cgColor
+        progressShape.strokeEnd = CGFloat(percent)
+        
+        let animation = CABasicAnimation(keyPath: "strokeEnd")
+        animation.fromValue = progressShape.strokeEnd
+        animation.toValue = percent/100.0
+        animation.duration = 2.5
+        animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut);
+        
+        if isAnimated {
+            progressShape.add(animation, forKey: nil)
+        }
     }
-
-
 }
 
